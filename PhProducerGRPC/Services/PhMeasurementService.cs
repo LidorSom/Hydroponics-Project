8c7894bd88a082435a6a044df8c715d6a70f8b2f
Lidor <lidor8199@gmail.com>
2023-03-05 17:38:50 +0200

    gRPCcommit

---
+++ PhProducerGRPC/Services/PhMeasurementService.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Core;
+
+namespace PhProducerGRPC.Services
+{
+    public class PhMeasurementService : PhProducerGRPC.PhMeasurementService.PhMeasurementServiceBase
+    {
+        public PhMeasurementService() { }
+
+        public override Task<PhMessageResponse> PhMeasurement(PhMessage request, ServerCallContext context)
+        {
+            return Task.FromResult<PhMessageResponse>(new PhMessageResponse() { Ok= true , PhValueGot = request.PhValue});
+        }
+    }
+}
